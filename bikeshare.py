e8fd78af4582eecd10cc292b23c4bd5b55c096c6
precious <oluphemmyprecious@gmail.com>
2021-05-20 11:55:47 +0100

    Added comments to bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -218,11 +218,16 @@ def user_stats(df):
 
     #Function to display 5 rows of raw data
 def data_display(df):
-    """Displays raw csv files of the data on user prompt."""
+    """Displays raw csv files of the data on user prompt.
+     user is prompted to enter 1 or 2 equivalent
+
+     to yes or no respectively
+    """
 
 
     rdata = ''
     count = 0
+# loop to determine if user wants to see the raw data that was analyzed
     while True:
         print("\nDo you wish to view the raw data?\nType '1' for YES and '2' for NO")
         rdata = int(input("Enter number here: "))
@@ -236,7 +241,7 @@ def data_display(df):
         else:
             print("\nKindly Enter appropriate number please")
 
-
+#loop to determine whether user wants to see more data or not
     while rdata == 'yes':
         print("\nDo you want to see more raw data?")
         count += 5
@@ -256,13 +261,13 @@ def main():
     while True:
         city, month, day = get_filters()
         df = load_data(city, month, day)
-
+#calling all defined functions above to enable them work
         time_stats(df)
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
         data_display(df)
-
+#prompt user to decide if he wants to run the script again
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
